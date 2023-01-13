6442bf680ffc33c709808fe9698b501a2c67c30f
Shelton Francisco <sheltonfjs@gmail.com>
2023-01-13 17:52:56 +0200

    feat: remove by id

--- controllers/TaskController.js
+++ controllers/TaskController.js
@@ -3,7 +3,7 @@ const Task = require("../models/Task");
 const getAllTasks = async (req, res) => {
   try {
     const taskList = await Task.find();
-    return res.render("index", {taskList});
+    return res.render("index", { taskList, task: null, taskDelete: null });
   } catch (err) {
     return res.status(500).send({ message: err.message });
   }
@@ -24,7 +24,45 @@ const createTask = async (req, res) => {
   }
 };
 
+const getById = async (req, res) => {
+  try {
+    const task = await Task.findOne({ _id: req.params.id });
+    const taskList = await Task.find();
+    const method = req.params.method;
+
+    if (method == "update") {
+      return res.render("index", { task, taskList, taskDelete: null });
+    } else {
+      return res.render("index", { task: null, taskList, taskDelete: task });
+    }
+  } catch (err) {
+    return res.status(500).send({ message: err.message });
+  }
+};
+
+const updateById = async (req, res) => {
+  try {
+    const task = req.body;
+    await Task.updateOne({ _id: req.params.id }, task);
+    return res.redirect("/");
+  } catch (err) {
+    return res.status(500).send({ message: err.message });
+  }
+};
+
+const deleteById = async (req, res) => {
+  try {
+    await Task.remove({ _id: req.params.id });
+    return res.redirect("/");
+  } catch (err) {
+    return res.status(500).send({ message: err.message });
+  }
+};
+
 module.exports = {
   getAllTasks,
   createTask,
+  getById,
+  updateById,
+  deleteById,
 };
--- routes/routes.js
+++ routes/routes.js
@@ -3,5 +3,9 @@ const taskController = require("../controllers/TaskController");
 
 routes.get("/", taskController.getAllTasks);
 routes.post("/create", taskController.createTask);
+routes.get("/getById/:id/:method", taskController.getById);
+routes.post("/updateTask/:id", taskController.updateById);
+routes.get("/deleteTask/:id", taskController.deleteById);
+
 
 module.exports = routes;
